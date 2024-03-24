8471994e665b1cf3d337af7593bccd46328df02c
heloabrantessb <heloisa.abrantesdev@gmail.com>
2024-03-24 13:21:02 -0300

    Criação de nova coluna na table points

--- src/database/knex/migrations/20240313173235_points.js
+++ src/database/knex/migrations/20240313173235_points.js
@@ -2,6 +2,7 @@ exports.up = (knex) => {
     return knex.schema.createTable('points', (table) => {
         table.increments('id').primary();
         table.string('name').notNullable();
+        table.text('summary').notNullable();
         table.text('description').notNullable();
         table.string('location').notNullable();
         table.float('mediaRating').notNullable();
