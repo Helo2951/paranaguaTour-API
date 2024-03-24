11ec2aa2fa2db94575d9eee987837f718679aa75
heloabrantessb <heloisa.abrantesdev@gmail.com>
2024-03-24 12:28:53 -0300

    mudança de tipo de campo

--- src/database/knex/migrations/20240313173235_points.js
+++ src/database/knex/migrations/20240313173235_points.js
@@ -2,7 +2,7 @@ exports.up = (knex) => {
     return knex.schema.createTable('points', (table) => {
         table.increments('id').primary();
         table.string('name').notNullable();
-        table.string('description').notNullable();
+        table.text('description').notNullable();
         table.string('location').notNullable();
         table.float('mediaRating').notNullable();
         table.string('category').notNullable();
